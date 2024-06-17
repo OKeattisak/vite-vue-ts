1954e9e8e4db07a6f702e3f8757a53f5a118e58f
Keattisak <OKeattisak.A@gmail.com>
2024-06-17 07:42:56 +0700

    Add Axios

--- src/components/Gift.vue
+++ src/components/Gift.vue
@@ -1,11 +1,62 @@
 <script setup lang="ts">
+import axios from 'axios';
+import { onMounted, ref } from 'vue'
 
+interface Product {
+    id: number;
+    productName: string;
+    productImage: string;
+    product: string;
+    price: number;
+}
+
+const isLoading = ref(true)
+const products = ref<Product[]>([]);
+console.log(isLoading.value)
+
+onMounted(() => {
+
+    try {
+        axios.get('https://666f7a4bf1e1da2be522e3a2.mockapi.io/products').then((response) => {
+            isLoading.value = false
+            console.log(isLoading.value)
+            console.log(response.data)
+            products.value = response.data
+        }).catch((error) => {
+            console.log(error)
+        }).finally(function () {
+            // always executed
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
 </script>
 
 <template>
     <v-container>
-        
+        <v-row>
+            <v-col v-for="product in products" :key="product.id" cols="6" md="3">
+                <v-card class="mx-auto">
+                    <v-img height="150px" :src="product.productImage" cover></v-img>
+                    <v-card-text class="product-detail">
+                        {{ product.productName }}
+                    </v-card-text>
+
+                    <v-badge color="primary" inline :content="product.product"></v-badge>
+
+                    <v-card-actions class="product-detail">
+                        {{ product.price }} พอยท์
+                        <v-spacer></v-spacer>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
-<style scoped></style>
+<style scoped>
+.product-detail {
+    font-size: 14px;
+}
+</style>
